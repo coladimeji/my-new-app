49d4706e0d6e9ffdb83025ff13a38ca40577f67f
coladimeji <coladimeji@gmail.com>
2021-10-14 12:21:04 -0400

    added tweetlike

---
+++ src/components/TweetLike.js
@@ -0,0 +1,11 @@
+function TweetLike () {
+    return(
+        <div   className='card'>
+  <h2>TITLE</h2>
+  <div   className='action'>
+  <button>Like</button>
+  </div>
+  </div>
+    );
+}
+export default TweetLike;
